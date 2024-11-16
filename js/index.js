6bc63ce9b03dabaeec89a80ddceedb3163bdcb96
Aizenda <qaz098380740669@gmail.com>
2024-11-16 13:21:04 +0800

    問題二回答

--- js/index.js
+++ js/index.js
@@ -1,12 +1,11 @@
-
-function chang1(){
+const chang1 = ()=>{
     let aboutDiv = document.querySelector("#about");
     let ApplicationMotivationDiv = document.querySelector("#Application-motivation");
     aboutDiv.style.display = "flex";
     ApplicationMotivationDiv.style.display = "none";
 };
 
-function chang2(){
+const chang2 = ()=>{
     let aboutDiv = document.querySelector("#about");
     let ApplicationMotivationDiv = document.querySelector("#Application-motivation");
     aboutDiv.style.display = "none";
@@ -14,7 +13,7 @@ function chang2(){
 
 };
 
-function FrontEnd(){
+const FrontEnd = ()=>{
     let htmlDiv = document.querySelector("#Front-end");
     let nodeDiv = document.querySelector("#Back-end");
     let pythonDiv = document.querySelector("#python");
@@ -26,7 +25,7 @@ function FrontEnd(){
 
 };
 
-function BackEnd(){
+const BackEnd = ()=>{
     let htmlDiv = document.querySelector("#Front-end");
     let nodeDiv = document.querySelector("#Back-end");
     let pythonDiv = document.querySelector("#python");
@@ -37,7 +36,7 @@ function BackEnd(){
     other.style.display= "none";
 };
 
-function python(){
+const python = ()=>{
     let htmlDiv = document.querySelector("#Front-end");
     let nodeDiv = document.querySelector("#Back-end");
     let pythonDiv = document.querySelector("#python");
@@ -49,7 +48,7 @@ function python(){
 
 };
 
-function other(){
+const other = ()=>{
     let htmlDiv = document.querySelector("#Front-end");
     let nodeDiv = document.querySelector("#Back-end");
     let pythonDiv = document.querySelector("#python");
@@ -59,3 +58,44 @@ function other(){
     pythonDiv.style.display= "none";
     other.style.display= "block";
 };
+
+document.addEventListener("DOMContentLoaded", ()=> {
+    // 1. 取得所有小圖片和 modal 元素
+    const thumbnails = document.querySelectorAll('.image-thumbnail');
+    const modal = document.createElement('div');
+    const modalImage = document.createElement('img');
+
+    // 2. 設定 modal 元素的基本樣式
+    modal.classList.add('modal');
+    modal.appendChild(modalImage);
+    document.body.appendChild(modal);
+
+    // 3. 為每張縮圖添加點擊事件
+    thumbnails.forEach(function(thumbnail) {
+        thumbnail.addEventListener('click', ()=> {
+            // 取得大圖的 URL
+            const fullSizeUrl = thumbnail.getAttribute('data-fullsize');
+            // 設置 modal 顯示的圖片
+            modalImage.src = fullSizeUrl;
+
+            // 動態修改圖片大小，取消全局 img 的樣式
+            modalImage.style.width = 'auto';   // 取消限制寬度
+            modalImage.style.height = 'auto';  // 取消限制高度
+            modalImage.style.maxWidth = '95%'; // 設置最大寬度
+            modalImage.style.maxHeight = '95%';// 設置最大高度
+            modalImage.style.padding = '0';     // 移除 padding
+
+            // 顯示 modal
+            modal.classList.add('show');
+        });
+    });
+
+    // 4. 點擊 modal 任何地方關閉放大圖片
+    modal.addEventListener('click', function() {
+        modal.classList.remove('show');
+    });
+});
+
+
+
+
